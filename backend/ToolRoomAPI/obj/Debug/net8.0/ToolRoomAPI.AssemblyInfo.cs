292db0921ef03ed8b429b1cb0f656f253aa81491
serkanmalkoc <234212032@ogrenci.amasya.edu.tr>
2026-05-24 19:15:50 +0300

    şifre güvenliği

--- backend/ToolRoomAPI/obj/Debug/net8.0/ToolRoomAPI.AssemblyInfo.cs
+++ backend/ToolRoomAPI/obj/Debug/net8.0/ToolRoomAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToolRoomAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54677541ff3bbe30f1d8ebab684293e63effd332")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToolRoomAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToolRoomAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
